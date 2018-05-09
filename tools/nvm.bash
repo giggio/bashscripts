f595273e1cbaf44d0d248683e82cb3002ed9fa34
Giovanni Bassi <giggio@giggio.net>
2018-05-09 17:50:51 +0000

    Lazy load nvm

--- tools/nvm.bash
+++ tools/nvm.bash
@@ -1,3 +1,7 @@
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# lazy load nvm, which is slow to start, so we dont 'use' at first, and alias node and npm to 'use'
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use # This loads nvm without use
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm_use () { nvm use --silent default; }
+alias node='unalias node; unalias npm; nvm_use; node $@'
+alias npm='unalias node; unalias npm; nvm_use; npm $@'
