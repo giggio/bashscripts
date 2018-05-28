03789b1379b0cb91c7abcc027b3b55729f096f54
Giovanni Bassi <giggio@giggio.net>
2018-05-28 21:10:12 +0000

    Add azds

--- bashrc-wsl.bash
+++ bashrc-wsl.bash
@@ -2,3 +2,7 @@ if ! [[ $(cat /proc/version) = *"Microsoft"* ]]; then
   return
 fi
 # put your script here for WSL only
+export whome=$(wslpath -u $(cmd.exe /c "echo %USERPROFILE%") | sed -e 's/[[:space:]]*$//')
+if [ "$whome" == "$(pwd)" ]; then
+  cd ~
+fi
---
+++ completions/azds.bash
@@ -0,0 +1,3 @@
+if [ -f  /etc/bash_completion.d/azds ]; then
+  source /etc/bash_completion.d/azds
+fi
