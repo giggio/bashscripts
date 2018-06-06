b59ffa681b2282e6238b05bd1792975186ccbe88
Giovanni Bassi <giggio@giggio.net>
2018-06-06 00:35:52 +0000

    Add azds

--- aliases/custom.aliases.bash
+++ aliases/custom.aliases.bash
@@ -20,16 +20,25 @@ alias l='ls -CF'
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 alias add='git add'
+complete -F _complete_alias add
 alias st='git status'
+complete -F _complete_alias st
 alias log='git log'
+complete -F _complete_alias log
 alias ci='git commit'
+complete -F _complete_alias ci
 alias push='git push'
+complete -F _complete_alias push
 alias pull='git pull'
+complete -F _complete_alias pull
 alias fixup='git fixup'
+complete -F _complete_alias fixup
 alias dif='git diff'
+complete -F _complete_alias dif
 if hash hub 2>/dev/null; then
   alias git=hub
 fi
 if hash kubectl 2>/dev/null; then
   alias k=kubectl
+  complete -F _complete_alias k
 fi
