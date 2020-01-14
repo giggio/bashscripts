80146378259388997a5a1a3e6d7fa950e2b35bea
Giovanni Bassi <giggio@giggio.net>
2020-01-14 12:24:08 -0300

    Add locale

--- bashrc.bash
+++ bashrc.bash
@@ -61,6 +61,7 @@ fi
 export GPG_TTY=$(tty)
 export N_PREFIX=$HOME/.n
 export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+export LC_ALL=en_US.UTF-8
 set -o vi
 bind '"jj":"\e"'
 
