162efca3b57e20acd4980187cd22209688cb0605
Giovanni Bassi <giggio@giggio.net>
2020-01-23 13:56:37 -0300

    Add fzf

---
+++ tools/fzf.bash
@@ -0,0 +1,15 @@
+if ! [ -d "$HOME/.fzf" ]; then return; fi
+
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */$HOME/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/$HOME/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/$HOME/.fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/$HOME/.fzf/shell/key-bindings.bash"
