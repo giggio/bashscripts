64835638a89c4aaac2a47fa707ee31593f196151
Giovanni Bassi <giggio@giggio.net>
2020-01-22 22:25:20 -0300

    Ignore .bak for kubeconfig

--- tools/k8s.bash
+++ tools/k8s.bash
@@ -1,3 +1,3 @@
 if [ -d "$HOME/.kube" ]; then
-  export KUBECONFIG=$HOME/.kube/config:$(find $HOME/.kube -maxdepth 1 -type f ! -name *.backup ! -name config ! -name kubectx |  paste -sd ":" -)
+  export KUBECONFIG=$HOME/.kube/config:$(find $HOME/.kube -maxdepth 1 -type f ! -name *.bak ! -name *.backup ! -name config ! -name kubectx |  paste -sd ":" -)
 fi
