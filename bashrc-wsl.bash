feba86db4c452f32d0be85ea1fb2db75c6587272
Giovanni Bassi <giggio@giggio.net>
2020-04-18 20:45:49 -0300

    Start cron

--- bashrc-wsl.bash
+++ bashrc-wsl.bash
@@ -24,6 +24,10 @@ if [ "$WSLVersion" == "1" ]; then
   fi
 fi
 pushd /mnt/c > /dev/null
+# start cron, add '%sudo ALL=NOPASSWD: /etc/init.d/cron start' via visudo if this fails
+if ! pgrep cron > /dev/null; then
+  sudo /etc/init.d/cron start > /dev/null
+fi
 export WHOME=$(wslpath -u $(cmd.exe /c "echo %USERPROFILE%") | sed -e 's/[[:space:]]*$//')
 popd > /dev/null
 if [ "$WHOME" == "$(pwd)" ]; then
