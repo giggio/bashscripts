567704e9baa3e9730e77626cce319efe780d583a
Giovanni Bassi <giggio@giggio.net>
2020-04-19 08:38:57 -0300

    Add checksum to clean memory script
    As this file runs as root, we have to checksum it,
    so if it is changed, the attacker cannot run as root.

--- cleanupMemoryCache.sh
+++ cleanupMemoryCache.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# if on WSL2 add this to a cron to run every minute
-# to limit the number of the log file run:
+# If on WSL2 add this to the root cron to run every minute, but be safe,
+# first run a checksum with `sudo ./cleanupMemoryCache.sh -c`
+# and on the root cron, add:
+# sha256sum --status --strict -c /root/.cleanupMemoryCache-sha256sum && /path/to/bashscripts/cleanupMemoryCache.sh
+# this will guarantee that if the file is tampered with it will not run anymore.
+#
+# To limit the number of the log file run:
 # echo "$(tail -n 20 /var/log/cacheclean)" > /var/log/cacheclean
+# It is also a good idea to run it every couple of hours or everyday.
+
+set -euo pipefail
 
 DATE_FORMAT=+%a\ %F\ %T\ %Z
 if [ "$(whoami)" != "root" ]; then
@@ -13,6 +21,13 @@ FORCE=false
 if [ "$#" -ge 1 ]; then
   if [ "$1" == '--force' ] || [ "$1" == '-f' ]; then
     FORCE=true
+  fi
+  if [ "$1" == '--generate-checksum' ] || [ "$1" == '-c' ]; then
+    THIS_DIR=`realpath $(dirname $BASH_SOURCE)`
+    CHECKSUM_FILE=$HOME/.cleanupMemoryCache-sha256sum
+    sha256sum $THIS_DIR/cleanupMemoryCache.sh > $CHECKSUM_FILE
+    echo "Generated checksum to '$CHECKSUM_FILE'."
+    exit 0
   fi
 fi
 CACHED="$(cat /proc/meminfo | grep ^Cached | awk '{print $2}')"
