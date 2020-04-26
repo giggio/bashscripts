062b770ac9054196ffd5a5dd98ef45b5cee8cfb5
Giovanni Bassi <giggio@giggio.net>
2020-04-26 16:07:36 -0300

    Add more aliases for wsl

--- aliases/wsl.aliases.bash
+++ aliases/wsl.aliases.bash
@@ -1,6 +1,9 @@
 # these are aliases that only exist if you are on WSL
 if ! $WSL; then return; fi
-alias explorer='explorer.exe'
-alias cmd='cmd.exe'
-alias powershell='powershell.exe'
+alias explorer=explorer.exe
+alias cmd=cmd.exe
+alias powershell=powershell.exe
 alias start='pwsh.exe -NoProfile -NoLogo -c start'
+alias clip=clip.exe
+alias notepad=notepad.exe
+alias wt=wt.exe
