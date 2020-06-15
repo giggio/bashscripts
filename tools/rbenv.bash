629cd71ebde717545a021c13be15f8939c1f2c13
Giovanni Bassi <giggio@giggio.net>
2020-06-15 00:28:26 -0300

    Add rbenv shim dirs to path

--- tools/rbenv.bash
+++ tools/rbenv.bash
@@ -1,5 +1,5 @@
 if [ -f $HOME/.rbenv/bin/rbenv ]; then
-  export PATH="$HOME/.rbenv/bin:$PATH"
+  export PATH="$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
   addCompletion $COMPLETIONS_DIR/rbenv
   updateCompletionsCommands="$updateCompletionsCommands\nrbenv init - > $COMPLETIONS_DIR/rbenv"
 fi
