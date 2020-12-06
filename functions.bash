6c92697e787ef2b122a3cafc7e1ff91803b43639
Giovanni Bassi <giggio@giggio.net>
2020-12-05 23:57:30 -0300

    Add function to edit variable

--- functions.bash
+++ functions.bash
@@ -1,6 +1,7 @@
 function hasBinary {
   hash $1 2>/dev/null
 }
+
 function hasBinaryInLinux {
   if ! $WSL; then
     hasBinary $1
@@ -18,4 +19,17 @@ function man {
         command man "$@"
         ;;
     esac
+}
+
+function edit-var {
+  declare -n VAR=$1
+  if ! [ -v VAR ]; then
+    echo "Variable with name '${!VAR}' does not exist."
+    return
+  fi
+  local TMP=`mktemp`
+  echo "${VAR}" > $TMP
+  vim $TMP
+  eval "${!VAR}"'=`cat '$TMP'`'
+  rm $TMP
 }
