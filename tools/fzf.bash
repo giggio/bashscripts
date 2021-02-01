cdac1f0937675b78987603130eb6cae9b5c36b27
Giovanni Bassi <giggio@giggio.net>
2021-02-01 12:37:03 -0300

    Use fd with fzf if available

--- tools/fzf.bash
+++ tools/fzf.bash
@@ -1,15 +1,19 @@
 if ! [ -d "$HOME/.fzf" ]; then return; fi
 
 # Setup fzf
-# ---------
 if [[ ! "$PATH" == */$HOME/.fzf/bin* ]]; then
   export PATH="${PATH:+${PATH}:}/$HOME/.fzf/bin"
 fi
 
 # Auto-completion
-# ---------------
 [[ $- == *i* ]] && source "/$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
-# ------------
 source "/$HOME/.fzf/shell/key-bindings.bash"
+
+# Use fd if available
+if hash fd 2>/dev/null; then
+  export FZF_DEFAULT_COMMAND="fd --type file --color=always --exclude .git"
+  export FZF_DEFAULT_OPTS="--ansi"
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+fi
