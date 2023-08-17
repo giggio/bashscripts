6d5c67352c631a9621b92b32345d2cbde8baf71b
Giovanni Bassi <giggio@giggio.net>
2023-08-16 23:18:34 -0300

    Revert cargo completions fix
    It has been fixed in rustup.

--- completions/017_rustup.bash
+++ completions/017_rustup.bash
@@ -1,4 +1,3 @@
 addCompletion $COMPLETIONS_DIR/rustup
 addCompletion $COMPLETIONS_DIR/cargo
-# todo: remove sed from the end when https://github.com/rust-lang/rustup/issues/3407 is fixed
-updateCompletionsCommands="$updateCompletionsCommands\nif hash rustup 2>/dev/null; then rustup completions bash > $COMPLETIONS_DIR/rustup; rustup completions bash cargo > $COMPLETIONS_DIR/cargo; sed -i 's/\\/etc\\/bash_completion.d\\//\\/src\\/etc\\/bash_completion.d\\//' $COMPLETIONS_DIR/cargo; fi"
+updateCompletionsCommands="$updateCompletionsCommands\nif hash rustup 2>/dev/null; then rustup completions bash > $COMPLETIONS_DIR/rustup; rustup completions bash cargo > $COMPLETIONS_DIR/cargo; fi"
