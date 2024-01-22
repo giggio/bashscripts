da72f4d76bf1784e85c58d55cd08e2d2f0147627
Giovanni Bassi <giggio@giggio.net>
2024-01-22 04:04:51 -0300

    Use cheats from dotfiles

--- tools/navi.bash
+++ tools/navi.bash
@@ -1,3 +1,9 @@
 if hash navi 2>/dev/null; then
   eval "$(navi widget bash)"
+  BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  CHEATS_DIR="$BASEDIR/../../cheats"
+  if [ -d "$CHEATS_DIR" ]; then
+    CHEATS_DIR=$(realpath "$CHEATS_DIR")
+    export NAVI_PATH="$CHEATS_DIR/dist/common/:$CHEATS_DIR/dist/bash/:$CHEATS_DIR/dist/linux/common/:$CHEATS_DIR/dist/linux/bash/"
+  fi
 fi
