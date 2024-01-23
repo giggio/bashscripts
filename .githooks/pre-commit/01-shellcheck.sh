f0501f596830ac0409a2b155885fbf6bd43a5067
Giovanni Bassi <giggio@giggio.net>
2024-01-23 20:17:40 -0300

    Add gh action, git hook and evolve shellcheck

--- .githooks/pre-commit/01-shellcheck.sh
+++ .githooks/pre-commit/01-shellcheck.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DIR="$(pwd)" # hooks are always ran from the root of the repo
 
 submodule_dirs=$(git submodule | awk '{ printf "-not -path '"$DIR"'/" $2 "/* " }')
 set -f # disable globbing
@@ -12,7 +12,5 @@ sh_files=$(find "$DIR" \( -name '*.sh' -or -name '*.bash' \) -not -path "$DIR"/.
 exec_files=$(find "$DIR" -not -path "$DIR"/.testsupport/'*' $submodule_dirs -type f -exec sh -c 'head -n1 $1 | grep -qE '"'"'^#!(.*/|\/usr\/bin\/env +)bash'"'" sh {} \; -exec echo {} \;)
 set +f
 all_files=`echo -e "$sh_files\n$exec_files" | sort | uniq`
-pushd "$DIR" > /dev/null
 # shellcheck disable=SC2086
 shellcheck --shell bash $all_files
-popd > /dev/null
