794683cd66389ef79bcd024820d60c21bb31bb8d
Giovanni Bassi <giggio@giggio.net>
2024-06-04 01:25:12 -0300

    Move alias completion to nix

--- completions/001_go.bash
+++
@@ -1,6 +0,0 @@
-if hash gocomplete 2>/dev/null; then
-  if GOCOMPLETE=`which gocomplete`; then
-    complete -C "$GOCOMPLETE" go
-  fi
-  unset GOCOMPLETE
-fi
--- completions/999_wrapup.bash
+++ completions/999_wrapup.bash
@@ -2,6 +2,3 @@ if ! [ -d "$COMPLETIONS_DIR" ]; then
   mkdir -p "$COMPLETIONS_DIR"
   updateCompletions
 fi
-
-# auto complete all aliases
-complete -F _complete_alias "${!BASH_ALIASES[@]}"
